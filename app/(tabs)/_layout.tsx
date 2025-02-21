81fc07576318ef6d678bb645ececf8577da95bdd
Jacobguttl <jacobguttl2006@gmail.com>
2025-02-21 11:22:42 +0100

    list

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,6 +4,7 @@ import { Platform } from 'react-native';
 
 import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
+import Feather from '@expo/vector-icons/Feather';
 import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
@@ -30,14 +31,14 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color }) => <Feather name="home" size={24} color="black" />,
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
           title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: ({ color }) => <Feather name="shopping-cart" size={24} color="black" />,
         }}
       />
     </Tabs>
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,111 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import {
+  Image,
+  StyleSheet,
+  Platform,
+  View,
+  Text,
+  Pressable,
+  FlatList,
+  ImageBackground,
+  Dimensions,
+} from "react-native";
+import { useState, useEffect } from "react";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
 
 export default function HomeScreen() {
+  const { width, height } = Dimensions.get("window");
+  const [meth, setMeth] = useState();
+
+  const ToExersise = () => {};
+
+  useEffect(() => {
+    const data = require('@/assets/data/products.json'); 
+    setMeth(data.product);
+  }, []);
+
+  type ImageMap = {
+    [key: string]: any;
+  };
+
+  const imageMap: ImageMap = {
+    meth2upscale: require("@/assets/images/meth2upscale.png"),
+    meth1: require("@/assets/images/meth1.png"),
+  };
+  
+
+
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require("@/assets/images/meth2upscale.png")}
           style={styles.reactLogo}
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
+      }
+    >
+      <View style={styles.titleContainer}>
+        <Text style={styles.titleText}>Crystalized</Text>
+      </View>
+
+      <View style={styles.container}>
+        <FlatList
+          style={""}
+          numColumns={2}
+          data={meth}
+          renderItem={({ item }) => (
+            <Pressable onPress={() => ToExersise()} style={styles.boxes}>
+              <ImageBackground
+                style={styles.backgroudImages}
+                source={imageMap[item.image.split('.')[0]]}
+              >
+                <Text style={styles.mediumText}>{`${item.name}, @/assets/images/${item.image}`}</Text>
+              </ImageBackground>
+            </Pressable>
+          )}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
     </ParallaxScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+    margin: "auto",
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  boxes: {
+    width: 300,
+    height: 300,
+    backgroundColor: "grey",
+    borderStyle: "solid",
+    borderWidth: 1,
+    borderColor: "black",
+    justifyContent: "center",
+    margin: 200,
+  },
+  mediumText: {
+    fontSize: 20,
+    textAlign: "center",
+    color: "white",
+  },
+  container: {
+    alignItems: "center",
+  },
+  titleText: {
+    fontSize: 40,
+  },
+  backgroudImages: {
+    height: "100%",
+    width: "100%",
+    justifyContent: "center",
   },
   reactLogo: {
-    height: 178,
-    width: 290,
+    height: "100%",
+    width: "100%",
     bottom: 0,
     left: 0,
-    position: 'absolute',
+    position: "absolute",
+    resizeMode: "cover",
   },
 });
